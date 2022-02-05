b66be922dd8903beeb4d5e9a4be8aa50769774ac
mihailkulakov <mihailkulakov@mail.ru>
2022-02-05 22:47:57 +0300

    Дз, кажется, готово.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template, send_from_directory
-# from functions import ...
+from flask import Flask, request, render_template, send_from_directory, redirect
+
+from functions import get_posts_by_tag, get_all_tags_from_posts
 
 POST_PATH = "posts.json"
 UPLOAD_FOLDER = "uploads/images"
@@ -9,17 +10,40 @@ app = Flask(__name__)
 
 @app.route("/")
 def page_index():
-    pass
+
+    tags = get_all_tags_from_posts()
+    return render_template("index.html", tags=tags)
 
 
 @app.route("/tag")
 def page_tag():
-    pass
+    tag_name = request.args.get('tag')
+    posts = get_posts_by_tag(tag_name)
+    return render_template("post_by_tag.html", tag=tag_name, posts=posts)
 
 
-@app.route("/post", methods=["GET", "POST"])
+
+@app.route("/post", methods=["GET"])
+def page_post_form():
+    return render_template("post_form.html")
+
+
+
+@app.route("/post", methods=["POST"])
 def page_post_create():
-    pass
+    picture = request.files.get("picture")
+    content = request.values.get("content")
+
+    if not picture:
+        return f"Ошибка загрузки"
+
+    filename = picture.filename
+    path = "./"+UPLOAD_FOLDER+"/"+filename
+
+    picture.save(path)
+
+    picture_new_url = "/"+UPLOAD_FOLDER+"/"+filename
+    return render_template("post_uploaded.html", picture=picture_new_url, content=content)
 
 
 @app.route("/uploads/<path:path>")
@@ -27,5 +51,7 @@ def static_dir(path):
     return send_from_directory("uploads", path)
 
 
-app.run()
 
+
+if __name__ == "__main__":
+    app.run()
--- functions.py
+++ functions.py
@@ -1,1 +1,46 @@
 import json
+
+def _load_posts_from_json():
+
+    with open("posts.json", "r", encoding="utf-8") as fp:
+        posts = json.load(fp)
+    if posts:
+        return posts
+    return []
+
+
+def get_posts_by_tag(tag_name):
+
+    posts = _load_posts_from_json()
+
+    posts_matching = []
+
+    search_for = f"#{tag_name}"
+
+    for post in posts:
+        if search_for in post.get("content"):
+            posts_matching.append(post)
+
+    return posts_matching
+
+
+def _get_all_tags_from_str(string):
+
+    tags = set()
+
+    for word in string.split(" "):
+        if word.startswith("#"):
+            tags.add(word[1:])
+    return tags
+
+
+def get_all_tags_from_posts():
+    posts = _load_posts_from_json()
+    tags = set()
+
+    for post in posts:
+        post_content = post.get("content")
+        tags_in_posts = _get_all_tags_from_str(post_content)
+        tags = tags.union(tags_in_posts)
+
+    return list(tags)
